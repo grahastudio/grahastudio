c8280ee90b7bba6abf85744ead02728ff7b6ee1f
edi-prasetyo <ediprasetiyo2@gmail.com>
2020-08-24 12:16:06 +0700

    update sitemap

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = 'oops';
 $route['translate_uri_dashes'] = FALSE;
+$route['sitemap\.xml'] = 'sitemap';
