a3a7e80331b8826e669a73a040603db7675d8276
edi-prasetyo <ediprasetiyo2@gmail.com>
2021-05-07 23:46:44 +0700

    Update New Version

--- application/views/front/page/index_about.php
+++ application/views/front/page/index_about.php
@@ -1,32 +1,32 @@
 <!-- ======= Breadcrumbs Section ======= -->
 <section class="breadcrumbs">
-    <div class="container">
+  <div class="container">
 
-      <div class="d-flex justify-content-between align-items-center">
-        <h2><?php echo $title ?></h2>
-        <ol>
-          <li><a href="<?php echo base_url('') ?>"><i class="ti ti-home"></i> Home</a></li>
-          <li><?php echo $title ?></li>
-        </ol>
-      </div>
-
+    <div class="d-flex justify-content-between align-items-center">
+      <h2><?php echo $title ?></h2>
+      <ol>
+        <li><a href="<?php echo base_url('') ?>"><i class="ti ti-home"></i> Home</a></li>
+        <li><?php echo $title ?></li>
+      </ol>
     </div>
-  </section><!-- End Breadcrumbs Section -->
+
+  </div>
+</section><!-- End Breadcrumbs Section -->
 
 <div class="margin-top container">
-    <div class="col-md-9">
-        <div class="card">
-            <div class="card-body">
-                <h2>About Us</h2>
-                <p>Graha studio adalah Jasa layanan yang bergerak di bidang digital dan teknologi. Kami melayani berbagai macam kebutuhan
-                    pemasaran anda secara online dan digital, Seperti Pembuatan Website, Video Animasi, Desain Grafis dll.
-                </p>
-                <p>Graha Studio akan memberikan inovasi dan dapat menjadi konsultan untuk mengembangkan bisnis anda.</p>
-                <p>
-                    Jika Bisnis anda saat ini di jalankan dengan cara pemasaran Offline mulailah berkembang dengan layanan yang kami berikan
+  <div class="col-md-9">
+    <div class="card">
+      <div class="card-body">
+        <h2>About Us</h2>
+        <p>Graha studio adalah Jasa layanan yang bergerak di bidang digital dan teknologi. Kami melayani berbagai macam kebutuhan
+          pemasaran anda secara online dan digital, Seperti Pembuatan Website, Video Animasi, Desain Grafis dll.
+        </p>
+        <p>Graha Studio akan memberikan inovasi dan dapat menjadi konsultan untuk mengembangkan bisnis anda melalui website.</p>
+        <p>
+          Jika Bisnis anda saat ini di jalankan dengan cara pemasaran Offline mulailah berkembang dengan layanan yang kami berikan
 
-                </p>
-            </div>
-        </div>
+        </p>
+      </div>
     </div>
+  </div>
 </div>
